7d64c036b47f2107cc16690e2e0eceac2a69d2e6
“Gabriel1610” <gabrielydeindependiente@gmail.com>
2024-09-27 22:59:54 -0300

    Cambios

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name = 'index'),
-    path('posts/', posts, name = 'posts'),
+    path('', include('apps.posts.urls')),
 ]
