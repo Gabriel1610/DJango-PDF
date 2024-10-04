c53ebccf3199bd68defdee3a4419c28f644f762a
“Gabriel1610” <gabrielydeindependiente@gmail.com>
2024-10-03 22:44:54 -0300

    Modificando url

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import posts
+# from .views import posts para poder llamar a una vista basada en funciones
+from .views import PostListView
+from . import views
 
 urlpatterns = [
-    path('posts/', posts, name = 'posts'),
+    # path('posts/', posts, name = 'posts'), forma de llamar a una vista basada en funciones
+    path('posts/', PostListView.asview(), name = 'posts'), # forma de llamar a una vista basada en funciones
 ]
