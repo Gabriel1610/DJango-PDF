3b3f433d934556e08dc611c7782ac57e72f829ff
“Gabriel1610” <gabrielydeindependiente@gmail.com>
2024-10-03 22:12:24 -0300

    Cambiando views.py

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
+# vista basada en funciones
 def posts(request):
     posts = Post.objects.all()
     return render(request, 'posts.html', {'posts':posts})
+
+# vista basada en clases
+class PostListView(ListView):
+    model = Post
+    template_name = "post.html"
+    context_object_name = "posts"
