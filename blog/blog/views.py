17fd99727724bf83a8d64f6432561bdce89be63f
“Gabriel1610” <gabrielydeindependiente@gmail.com>
2024-10-06 20:21:48 -0300

    Modificando post

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    current_url = request.path
-    context = {
-        'current_url': current_url,
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
