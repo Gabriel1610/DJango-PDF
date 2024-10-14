5c35c1d4e2a435380675c68f8bf9f3f6e5e6f8ff
royleguiza <leguizavictorroy@gmail.com>
2024-10-13 21:24:28 -0300

    continuando con la edicion

--- blog/blog/configuraciones/local.py
+++ blog/blog/configuraciones/local.py
@@ -1,1 +1,34 @@
 from .settings import *
+import os
+
+ 
+
+# ... otras configuraciones si las tienes ...
+
+ 
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'blogdb',
+
+        'USER': 'root',
+
+        'PASSWORD': 'root',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '3306',
+
+    }
+
+}
+
+ 
+
+# Configura la clave secreta de forma segura
+
+#SECRET_KEY = os.environ.get('SECRET_KEY')
