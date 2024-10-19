6b256632ec32dad907bcc9716feb0ea4b42b95a0
royleguiza <leguizavictorroy@gmail.com>
2024-10-19 00:12:48 -0300

    Migraciones realizadas

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -35,7 +35,7 @@ class Post(models.Model):
 
 
 class Comentario(models.Model):
-    posts = models.ForeignKey('posts. Post', on_delete = models.CASCADE, related_name='comentarios')
+    posts = models.ForeignKey('posts.Post', on_delete = models.CASCADE, related_name='comentarios')
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models. CASCADE, related_name='comentarios')
     models.TextField()
     texto = models.TextField()
