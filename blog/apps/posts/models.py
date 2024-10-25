91477f05756d166bce33f4c3b3e25543c34b1e7e
royleguiza <leguizavictorroy@gmail.com>
2024-10-24 21:35:24 -0300

    funcionando crear post y categoria

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -14,7 +14,7 @@ class Categoria(models.Model):
         return self.nombre
     
 class Post(models.Model):
-    titulo = models.CharField(max_length=50, null=False)
+    titulo = models.CharField(max_length=100, null=False)
     subtitulo = models.CharField(max_length=100, null=True, blank=True)
     fecha = models.DateTimeField(auto_now_add=True)
     texto = models.TextField(null=False)
