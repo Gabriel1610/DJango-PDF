38e9b250044a6528d2f24e1fa7be2eeab04b6fc3
royleguiza <leguizavictorroy@gmail.com>
2024-10-27 23:35:52 -0300

    como esta activo en  pythonanyware

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from apps.posts.models import Post  # Asegúrate de que esta ruta sea correcta
 
 def index(request):
-    return render(request, 'index.html')
+    post_fijo = Post.objects.filter(id=5)  # Consulta el post con id=5
+    otros_posts = Post.objects.exclude(id=5)  # Excluye el post con id=5 y consulta el resto
+    posts = list(post_fijo) + list(otros_posts)  # Combina ambos conjuntos, con el post fijo primero
+    return render(request, 'index.html', {'posts': posts})
 
 def acercade(request):
     return render(request, 'acercade.html')
 
 def pagina_404(request, exception):
-    return HttpResponseNotFound('<h1>Página no encontrada </h1>')
+    return HttpResponseNotFound('<h1>Página no encontrada</h1>')
